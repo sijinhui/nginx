d81bede0d4a0329857d1f629d229df73f6b95834
sijinhui <sijinhui@qq.com>
2024-07-17 11:04:25 +0800

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ envsubst '${DOCKER_PROXY_SERVER_NAME}' < /etc/nginx/conf.d/docker-proxy.conf > /
 # shellcheck disable=SC2016
 envsubst '${JSD_PROXY_SERVER_NAME}' < /etc/nginx/conf.d/jsd-proxy.conf > /etc/nginx/conf.d/jsd-proxy.conf.tmp && mv /etc/nginx/conf.d/jsd-proxy.conf.tmp /etc/nginx/conf.d/jsd-proxy.conf
 # shellcheck disable=SC2016
-envsubst '${JEMINI_PROXY_SERVER_NAME}' < /etc/nginx/conf.d/jemini-proxy.conf > /etc/nginx/conf.d/jemini-proxy.conf.tmp && mv /etc/nginx/conf.d/jemini-proxy.conf.tmp /etc/nginx/conf.d/jemini-proxy.conf
+envsubst '${JEMINI_PROXY_SERVER_NAME}' < /etc/nginx/conf.d/gemini-proxy.conf > /etc/nginx/conf.d/gemini-proxy.conf.tmp && mv /etc/nginx/conf.d/gemini-proxy.conf.tmp /etc/nginx/conf.d/gemini-proxy.conf
 
 # 启动 nginx
 exec nginx -g 'daemon off;'
